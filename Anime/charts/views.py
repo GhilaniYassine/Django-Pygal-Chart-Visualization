f1a270510148fa3c58c5b7c5c3ca4c8934e2b1c9
GhilaniYassine <ghilaniyassine11@gmail.com>
2025-07-30 23:29:48 +0100

    Pie using Pygal done ,queryingthe db slow

--- Anime/charts/views.py
+++ Anime/charts/views.py
@@ -7,7 +7,23 @@ from .charts import AnimePieChart
 # get the anime names from  the database 
 anime_names = Anime.objects.values_list('name', flat=True)
 
-def home(request):
 
-    
-    return render(request, 'pie.html')
+class home(TemplateView):
+    template_name = 'pie.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(home, self).get_context_data(**kwargs)
+
+        # Instantiate our chart. We'll keep the size/style/etc.
+        # config here in the view instead of `charts.py`.
+        cht_employee = AnimePieChart(
+            height=600,
+            width=800,
+            explicit_size=True,
+            style=DarkStyle
+        )
+
+        # Call the `.generate()` method on our chart object
+        # and pass it to template context.
+        context['cht_employee'] = cht_employee.generate()
+        return context
